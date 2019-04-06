538ae9ee2c7063cc9d0846c345bc9e30ed359df3
Drew Kennedy <drewkenn@gmail.com>
2019-04-06 14:06:17 +1100

    Added support for external volume for system DBs

--- windows/mssql-server-windows-developer/start.ps1
+++ windows/mssql-server-windows-developer/start.ps1
@@ -3,30 +3,34 @@
 # The format for attach_dbs
 
 param(
-[Parameter(Mandatory=$false)]
-[string]$sa_password,
+    [Parameter(Mandatory = $false)]
+    [string]$sa_password,
 
-[Parameter(Mandatory=$false)]
-[string]$ACCEPT_EULA,
+    [Parameter(Mandatory = $false)]
+    [string]$ACCEPT_EULA,
 
-[Parameter(Mandatory=$false)]
-[string]$attach_dbs
+    [Parameter(Mandatory = $false)]
+    [string]$attach_dbs,
+
+    [Parameter(Mandatory = $false)]
+    [string]$DataVolume
 )
 
+Write-Verbose "VOLUME: $DataVolume"
 
-if($ACCEPT_EULA -ne "Y" -And $ACCEPT_EULA -ne "y")
-{
-	Write-Verbose "ERROR: You must accept the End User License Agreement before this container can start."
-	Write-Verbose "Set the environment variable ACCEPT_EULA to 'Y' if you accept the agreement."
+
+if ($ACCEPT_EULA -ne "Y" -And $ACCEPT_EULA -ne "y") {
+    Write-Verbose "ERROR: You must accept the End User License Agreement before this container can start."
+    Write-Verbose "Set the environment variable ACCEPT_EULA to 'Y' if you accept the agreement."
 
     exit 1
 }
 
 # start the service
 Write-Verbose "Starting SQL Server"
-start-service MSSQLSERVER
+NET START MSSQLSERVER /f /T3608
 
-if($sa_password -eq "_") {
+if ($sa_password -eq "_") {
     if (Test-Path $env:sa_password_path) {
         $sa_password = Get-Content -Raw $secretPath
     }
@@ -35,26 +39,106 @@ if($sa_password -eq "_") {
     }
 }
 
-if($sa_password -ne "_")
-{
+if ($sa_password -ne "_") {
     Write-Verbose "Changing SA login credentials"
-    $sqlcmd = "ALTER LOGIN sa with password=" +"'" + $sa_password + "'" + ";ALTER LOGIN sa ENABLE;"
+    $sqlcmd = "ALTER LOGIN sa with password=" + "'" + $sa_password + "'" + ";ALTER LOGIN sa ENABLE;"
     & sqlcmd -Q $sqlcmd
 }
+
+
+Get-ChildItem -Path $DataVolume | foreach {Remove-Item -Path $_.FullName -Force}
+
+$TSQL = "ALTER
+DATABASE msdb MODIFY
+FILE ( NAME = MSDBData , FILENAME
+=
+'$DataVolume\MSDBData.mdf')
+
+ALTER
+DATABASE msdb MODIFY
+FILE ( NAME = MSDBLog , FILENAME
+=
+'$DataVolume\MSDBLog.ldf')
+
+ALTER
+DATABASE model MODIFY
+FILE ( NAME = modeldev , FILENAME
+=
+'$DataVolume\model.mdf')
+
+ALTER
+DATABASE model MODIFY
+FILE ( NAME = modellog , FILENAME
+=
+'$DataVolume\modellog.ldf')
+
+ALTER
+DATABASE tempdb MODIFY
+FILE ( NAME = tempdev , FILENAME
+=
+'$DataVolume\temp.mdf')
+
+ALTER
+DATABASE tempdb MODIFY
+FILE ( NAME = templog , FILENAME
+=
+'$DataVolume\temp.ldf')
+
+SELECT name, physical_name AS CurrentLocation, state_desc
+
+FROM
+sys.master_files
+
+WHERE database_id in
+(DB_ID(N'master'),DB_ID(N'model'),DB_ID(N'msdb'));
+"
+
+#Invoke-Sqlcmd -Query $TSQL -ServerInstance ".\"
+& sqlcmd -Q $TSQL
+
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQLServer\Parameters"
+
+if (Test-Path -Path $RegPath ) {
+    Write-Verbose "'MSSQLServer\Parameters' Path exists"
+    $Parameters = Get-ItemProperty -Path $RegPath
+
+    $ParametersHash = @(
+        $Parameters.SQLArg0
+        $Parameters.SQLArg1
+        $Parameters.SQLArg2
+    )
+
+    $i = 0
+    foreach ($item in $ParametersHash) {
+        switch -Regex ($item) {
+            '-d' {Set-ItemProperty -Path $RegPath -Name "SQLArg$i" -Value "-d$DataVolume\master.mdf"}
+            '-l' {Set-ItemProperty -Path $RegPath -Name "SQLArg$i" -Value "-l$DataVolume\mastlog.ldf"}
+        }
+        $i++
+    }
+}
+else {
+    Write-Verbose "'MSSQLServer\Parameters' Path does not exist"
+}
+
+
+Stop-Service MSSQLSERVER
+
+Copy-Item -Path 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\*.*' -Destination "$DataVolume"
+
+Start-Service MSSQLSERVER
+
 
 $attach_dbs_cleaned = $attach_dbs.TrimStart('\\').TrimEnd('\\')
 
 $dbs = $attach_dbs_cleaned | ConvertFrom-Json
 
-if ($null -ne $dbs -And $dbs.Length -gt 0)
-{
+if ($null -ne $dbs -And $dbs.Length -gt 0) {
     Write-Verbose "Attaching $($dbs.Length) database(s)"
 	    
-    Foreach($db in $dbs) 
-    {            
+    Foreach ($db in $dbs) {            
         $files = @();
-        Foreach($file in $db.dbFiles)
-        {
+        Foreach ($file in $db.dbFiles) {
             $files += "(FILENAME = N'$($file)')";           
         }
 
@@ -63,14 +147,13 @@ if ($null -ne $dbs -And $dbs.Length -gt 0)
 
         Write-Verbose "Invoke-Sqlcmd -Query $($sqlcmd)"
         & sqlcmd -Q $sqlcmd
-	}
+    }
 }
 
 Write-Verbose "Started SQL Server."
 
 $lastCheck = (Get-Date).AddSeconds(-2) 
-while ($true) 
-{ 
+while ($true) { 
     Get-EventLog -LogName Application -Source "MSSQL*" -After $lastCheck | Select-Object TimeGenerated, EntryType, Message	 
     $lastCheck = Get-Date 
     Start-Sleep -Seconds 2 
